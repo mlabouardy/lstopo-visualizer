84c7846c382903202f0b067400a77d61ede42dca
khelixx <guillaume.verdugo@gmail.com>
2016-02-18 12:32:59 +0100

    We can now find all element of a simple package as topologie.xml

--- app/scripts/controllers/visualizer.js
+++ app/scripts/controllers/visualizer.js
@@ -10,47 +10,56 @@ angular.module('myApp')
 
         $scope.infoSocket = $scope.jsonObj.topology.object.object[0]._type+ " P#"+$scope.jsonObj.topology.object.object[0]._os_index; //Information sur le socket
 
-        $scope.cacheL3 = null //$scope.jsonObj.topology.object.object[0].object; //Attention! La variable contient encore le tableau avec tout les autres caches et les cores
+        //variables for the display
         $scope.entities = [];
+        $scope.cacheL3 = []; 
+        $scope.cacheL2 = [];
         $scope.cachesL1 = [];
         $scope.deep_core = "";
 
+        //use to browse items in the json file. We use it with the eval function to find them.
         var search = "$scope.jsonObj.topology.object";
         
+        //extract core with deep_core variable init in cacheL2.
         $scope.extractCores = function(){
-            var array = $scope.deep_core +"[i]";
-            console.log($scope.jsonObj.topology.object);
-            for(var i = 0; i<eval($scope.deep_core).length; i++){
-                $scope.cores.push((eval(array)).object.object.object);
-            }
+            $scope.cacheL2.forEach(function(cache,index){
+                var tab_tmp = [];
+                for(var i = 0; i<cache.length; i++){
+                    tab_tmp.push((eval($scope.deep_core))[i].object.object.object);
+                }
+                $scope.cores.push(tab_tmp);
+            });
         }
 
         //Contrairement à $scope.cacheL3 dans laquelle il y a trop d'informations pour les caches L2 la fonction récupère que 3 paramètres
         $scope.extractCachesL2 = function(){
-            var array = $scope.cacheL3;
-            var tmp_l2 = "array.object";
             search += ".object";
-            while(true){
-                if ((eval(tmp_l2)) instanceof Array){
+            $scope.deep_core = search;
+            var tmp_l2 = (eval(search));
+            $scope.entities.forEach(function(){
+                var tab_tmp = [];
+                //while child are not L2 we continue our search et we modify search to do it
+                while(true){
+                    //if L2 is an array
+                    if ((eval(tmp_l2)) instanceof Array){
                         for(var i = 0; i<(eval(tmp_l2)).length; i++){
-                             $scope.cacheL2.push({"_type":(eval(tmp_l2))[i]._type, "_depth":(eval(tmp_l2))[i]._depth, "_cache_size":(eval(tmp_l2))[i]._cache_size});
-                        }
-                    break;
-                }
-                else{
-                     if ((eval(tmp_l2))._depth == 2 && (eval(tmp_l2)) != null){
-                        $scope.cacheL2.push({"_type":(eval(tmp_l2))._type, "_depth":(eval(tmp_l2))._depth, "_cache_size":(eval(tmp_l2))._cache_size});
+                           tab_tmp.push({"_type":(eval(tmp_l2))[i]._type, "_depth":(eval(tmp_l2))[i]._depth, "_cache_size":(eval(tmp_l2))[i]._cache_size});
+                       }
+                       break;
+                   }
+                   else{
+                    //if L2 is just an object
+                       if ((eval(tmp_l2))._depth == 2 && (eval(tmp_l2)) != null){
+                        tab_tmp.push({"_type":(eval(tmp_l2))._type, "_depth":(eval(tmp_l2))._depth, "_cache_size":(eval(tmp_l2))._cache_size});
                         break;
-                     }
-                     else{
-                        tmp_l2 += ".object";
+                    }
+                    else{
                         search += ".object";
-                     }
+                    }
                 }
             }
-           /* for(var i = 0; i<array.length; i++){
-                $scope.cacheL2.push({"_type":array[i]._type, "_depth":array[i]._depth, "_cache_size":array[i]._cache_size});
-            }*/
+            $scope.cacheL2.push(tab_tmp);
+        });
         }
 
         $scope.convertSizeInKb = function(size){
@@ -59,7 +68,6 @@ angular.module('myApp')
 
         $scope.extractEntities = function(){
             var entities = (eval(search));
-
             if(entities instanceof Array){
                 for(var i=0; i<entities.length; i++){
                     $scope.entities.push(entities[i]);
@@ -70,48 +78,77 @@ angular.module('myApp')
             }
         }
 
+        // we extract all L3cache
         $scope.extractCacheL3 = function(){
-            console.log((eval(search)));
-            while(true){
-                if((eval(search)) instanceof Array){
-                    search += "[0]";
-                }
-                else{
-                    if ((eval(search))._depth == 3 && (eval(search)) != null){
-                        $scope.cacheL3 = (eval(search));
-                        break;
+            $scope.entities.forEach(function(entity,index){
+                console.log(entity);
+                while(true){
+                    //if is an array.
+                    if((eval(search)) instanceof Array){
+                        search += "[0]";
                     }
                     else{
-                        search += ".object";
-                    }
-                }
-            }
-
-             $scope.deep_core = search + ".object";
-        }
-
-        $scope.extractCachesL1Bis= function(){
-            var array = [];
-            var check  = true;
-            $scope.entities.forEach(function(entity,index){
-                entity.object[0].object.object.forEach(function(Core,index){
-                var tmpL1 = [];
-                var deep = "Core.object";
-                while(true){
-                    if((eval(deep)) != null && (eval(deep))._depth == 1){
-                        tmpL1.push(Core.object);
-                        deep += ".object";
-                        if (check == true){
+                        //we verify that current object have wanted properties
+                        if ((eval(search))._depth == 3 && (eval(search)) != null){
+                            $scope.cacheL3.push(eval(search));
+                            break;
+                        }
+                        else{
+                            search += ".object";
                         }
                     }
-                    else{
-                        check = false;
-                        break;
-                    }  
                 }
-                array.push({l1 : tmpL1});
-                });  
              });
+            //console.log($scope.cacheL3)
+        }
+
+        /* The more complicated function. We search in the json file:
+        *   - first L1cacheD
+        *   - then we search L1cacheI.
+        */   
+        $scope.extractCachesL1Bis= function(){
+            var array = [];
+             $scope.entities.forEach(function(){
+                //here i put l3[0] temporary
+                $scope.cacheL3[0].object.forEach(function(Core,index){
+                    var deep = "Core.object";
+                    var tmpL1 = [];
+                    while(true){
+                        if ((eval(deep)) instanceof Array){
+                            for(i = 0; i < eval(deep).length; i++){
+                                var tmp = [];
+                                var deep_array = deep + "[i]";
+                                while(true){
+                                    if((eval(deep_array))._depth != null && (eval(deep_array))._depth == 1){
+                                        tmp.push(eval(deep_array));
+                                        deep_array += ".object";
+                                    }
+                                    else{
+                                        break;
+                                    } 
+                                }
+                                tmpL1.push(tmp);
+                            }
+                            break;
+                        }
+
+                        else{
+                            var tmp = [];
+                            if((eval(deep)) != null && (eval(deep))._depth == 1){
+                                tmp.push(Core.object);
+                                deep += ".object";
+                            }
+                            else{
+                                break;
+                            }
+
+                            tmpL1 = tmp;  
+                        }
+                    }
+                    
+                    array.push(tmpL1);
+                });  
+            });
         }
 
         $scope.convertSizeInMb = function(size){
@@ -124,6 +161,5 @@ angular.module('myApp')
         $scope.extractCachesL2();
         $scope.extractCachesL1Bis();
         $scope.extractCores();
-        //$scope.extractCores($scope.jsonObj.topology.object.object[0].object.object);
     }
-);
+    );
