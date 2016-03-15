f09daf0c6e6194b7374d8da01188dc034bad213b
khelixx <guillaume.verdugo@gmail.com>
2016-03-15 20:05:47 +0100

    taille police

--- app/scripts/controllers/visualizer.js
+++ app/scripts/controllers/visualizer.js
@@ -13,6 +13,7 @@ angular.module('myApp')
         $scope.arrayPackages = [];
         $scope.arrayGroups = [];
         $scope.arrayNUMANodes = [];
+        $scope.font_size = 14;
         var i = 0;
 
         function extractDatas(datas, entities){
@@ -311,6 +312,15 @@ angular.module('myApp')
             }
           ]
         };
+        }
+
+        $scope.changeSize = function (ope){
+            if (ope =="+"){
+                $scope.font_size ++ ;
+            }
+            else{
+                $scope.font_size -- ;
+            }
         }
 
         $scope.exportConfig=function(){
